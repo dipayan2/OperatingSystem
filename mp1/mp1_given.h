fd248250fdb799418fe31bc0575a4176fe620da7
Dipayan Mukherjee <dipayan2@illinois.edu>
2021-02-20 21:52:55 -0600

    Check 2

--- mp1/mp1_given.h
+++ mp1/mp1_given.h
@@ -7,6 +7,7 @@
 #define find_task_by_pid(nr) pid_task(find_vpid(nr), PIDTYPE_PID)
 
 //THIS FUNCTION RETURNS 0 IF THE PID IS VALID AND THE CPU TIME IS SUCCESFULLY RETURNED BY THE PARAMETER CPU_USE. OTHERWISE IT RETURNS -1
+// This is embarrasing
 int get_cpu_use(int pid, unsigned long *cpu_use)
 {
    struct task_struct* task;
