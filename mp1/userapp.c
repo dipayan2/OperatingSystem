ed893ec72afd6a074e90f83687a92888dbb87970
Dipayan Mukherjee <dipayan2@illinois.edu>
2021-02-27 23:54:21 -0600

    UserApp done

--- mp1/userapp.c
+++ mp1/userapp.c
@@ -1,6 +1,16 @@
 #include "userapp.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
+	int mypid;
+	char buffer[60];
+	mypid = getpid();
+	sprintf(buffer, "echo %d > /proc/mp1/status", mypid);
+	system(buffer);
 	return 0;
 }
