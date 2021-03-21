e4c934e57b309884ee8e9cb78105c3bf23a82718
Dipayan Mukherjee <dipayan2@illinois.edu>
2021-03-21 15:06:36 -0500

    Read/Write/reg Done

--- mp2/mp2.c
+++ mp2/mp2.c
@@ -25,8 +25,13 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("dipayan2");
 MODULE_DESCRIPTION("CS-423 MP2");
 
+/* LOCKING VARIABLE*/
+static DEFINE_SPINLOCK(my_lock);
 
 enum task_state {READY = 0, RUNNING = 1, SLEEPING = 2}; 
+
+
+struct list_head test_head;
 
 struct mp2_task_struct {
   struct task_struct *linux_task;
@@ -52,12 +57,21 @@ void removeLeadSpace(char** ptr){
 }
 
 void handleRegistration(char *kbuf){
+
+   // Data for reading the input
    int idx = 0;
    char* token;
    long readVal[5];
    char action;
    char *endptr;
    long value;
+   long tperiod, comp_time;
+   int pid_inp;
+
+   // Data for creating and storing the process block
+   struct mp2_task_struct *task_inp;
+
+
    while( (token = strsep(&kbuf,",")) != NULL ){
          if (idx == 0){
             action = *token;
@@ -78,6 +92,30 @@ void handleRegistration(char *kbuf){
          }
       idx += 1;
    } // read all the data
+
+   pid_inp = (int)readVal[0];
+   tperiod = readVal[1];
+   comp_time = readVal[2];
+
+   // Need to use kmem. for now using kmallloo
+   task_inp = (struct mp2_task_struct *) kmalloc(sizeof(struct mp2_task_struct),GFP_KERNEL);
+   if (!task_inp){
+      printk(KERN_INFO "Unable to allocate pid_inp memory\n");
+      return -EFAULT;
+   }
+
+   task_inp->pid = pid_inp;
+   task_inp->period_ms = tperiod;
+   task_inp->runtime_ms = comp_time;
+   task_inp->linux_task = find_task_by_pid(pid_inp);
+   task_inp->state = SLEEPING;
+
+
+   // Add to list should be within lock
+   spin_lock(&my_lock);
+   list_add(&(task_inp->list),&test_head);
+   spin_unlock(&my_lock);
+
 
    // Do the work of registration
    
@@ -117,10 +155,15 @@ void handleYield(char *kbuf){
 void handleDeReg(char *kbuf){
    int idx = 0;
    char* token;
-   long t_pid;
+   int t_pid;
    char action;
    char *endptr;
    long value;
+
+   // Data for dereg the task
+   struct list_head *posv, *qv;
+   struct mp2_task_struct *temp;
+
    while( (token = strsep(&kbuf,",")) != NULL ){
       if (idx == 0){
          action = *token;
@@ -135,14 +178,29 @@ void handleDeReg(char *kbuf){
          } 
          else{
             printk(KERN_ALERT "The value is %ld\n", value);
-            t_pid = value;
+            t_pid = (int)value;
+            break;
          }
          
       }
       idx += 1;
    } // read all the data
 
-   // Do work of Yield
+   // Do work DeReg
+   // need to stop the timer, but let's not do that yet!!
+   // Just remove from the list
+   spin_lock(&my_lock);
+   list_for_each_safe(posv, qv, &test_head){
+
+		 temp= list_entry(posv, struct mp2_task_struct, list);
+       if (temp->pid == t_pid){
+          list_del(posv);
+          printk(KERN_INFO "\nDeleted Pid : %d and cpu_time\n", temp->my_id);
+          kfree(temp);
+       }
+	}
+   spin_unlock(&my_lock);
+
 }
 
  /**
@@ -181,54 +239,7 @@ static ssize_t mp2_write (struct file *file, const char __user *buffer, size_t c
     else if(kbuf[0]=='D'){
        handleDeReg(kbuf);
     }
-    //idx = 0;
-   //  while( (token = strsep(&kbuf,",")) != NULL ){
-   //    if (idx == 0){
-   //          action = *token;
-   //          printk(KERN_ALERT "This value is %c \n",action);
-   //       }
-   //       else{
-   //          if (action == 'R'){
-   //             if (idx <= 3){
-   //                removeLeadSpace(&token);
-   //                printk(KERN_ALERT "The token : %s\n",token);
-   //                value = simple_strtol(token, &endptr, 10);
-   //                if (value == 0 && endptr == token){
-   //                   printk(KERN_ALERT "Error in long conversion");
-   //                } 
-   //                else{
-   //                   printk(KERN_ALERT "The value is %ld\n", value);
-   //                   readVal[idx-1] = value;
-   //                }
 
-                  
-   //             }
-   //             else{
-   //                break;
-   //             }
-   //          }
-   //          else if (action == 'Y' || action == 'D'){
-        
-   //             removeLeadSpace(&token);
-   //             printk(KERN_ALERT "The token : %s\n",token);
-   //             value = simple_strtol(token, &endptr, 10);
-   //             if (value == 0 && endptr == token){
-   //                printk(KERN_ALERT "Error in long conversion");
-   //             } 
-   //             else{
-   //                printk(KERN_ALERT "The value is %ld\n", value);
-   //                readVal[idx-1] = value;
-   //             }
-                
-   //             break;
-   //          }
-   //       }
-   //       idx += 1;
-   //       //printk(KERN_ALERT "%s\n",token);
-   //  }
-        
-
-    // use strsep
     
     kfree(kbuf);
     return ret;
@@ -236,7 +247,47 @@ static ssize_t mp2_write (struct file *file, const char __user *buffer, size_t c
 
 // kernel to proc
 static ssize_t mp2_read (struct file *file, char __user *buffer, size_t count, loff_t *data){
-    return 0;
+
+     // Data declaration
+   int copied;                // Return Value
+   int len=0;                 // How much data is read
+   char *buf;                 // Kernel buffer to read data
+   struct list_head *ptr;     // Kernel Linked List 
+   struct mp2_task_struct *entry; // Kernel Linked List
+   // Checking if the offset is correct
+   if (*data > 0){
+      printk(KERN_INFO "\nRead offset issue\n");
+      return 0;
+   }
+   printk(KERN_INFO "\nRead function called\n");
+  
+   buf = (char *) kmalloc(count,GFP_KERNEL);    // Allocating kernel memory and checking if properly allocated
+   if (!buf){
+      printk(KERN_INFO "Unable to allocate buffer!!\n");
+       return -ENOMEM;
+   }
+      
+   copied = 0;
+   // Checking id list is empty otherwise, acquiring the lock and iterationg over and writing list values into the buffer.
+   if (!list_empty(&test_head)){
+      spin_lock(&my_lock);
+      list_for_each(ptr,&test_head){
+         entry=list_entry(ptr,struct mp2_task_struct,list);
+         //printk(KERN_INFO "\n PID %d:Time %lu  \n ", entry->my_id,entry->cpu_time);
+         // Add this entry into the buffer
+         len += scnprintf(buf+len,count-len,"%d: %lu, %lu\n",entry->pid,entry->period_ms, entry->runtime_ms);
+      }
+      spin_unlock(&my_lock);
+   }
+
+
+   // Sending data to user buffer from kernel buffer
+   copied = simple_read_from_buffer(buffer,count,data,buf,len);
+   if (copied < 0){
+      return -EFAULT;
+   }
+   kfree(buf);
+   return copied ;
 }
 /**
  * Proc Fs data structure 
@@ -258,7 +309,7 @@ int __init mp2_init(void)
    #endif
    printk(KERN_ALERT "Hello World\n");
    // Initilizing the linked list
-//    INIT_LIST_HEAD(&test_head);
+   INIT_LIST_HEAD(&test_head);
 //    // Created the directory
    mp2_dir = proc_mkdir("mp2", NULL);
    // Adding the files
@@ -287,21 +338,21 @@ void __exit mp2_exit(void)
 //    flush_workqueue( my_wq );
 //    destroy_workqueue( my_wq );
 //    // Remove the list
-//    struct list_head *pos, *q;
-//    struct my_pid_data *tmp;
+    struct list_head *pos, *q;
+    struct mp2_task_struct *tmp;
 
-//    spin_lock(&my_lock);
-//    if(!list_empty(&test_head)){
-//          list_for_each_safe(pos, q, &test_head){
+   spin_lock(&my_lock);
+   if(!list_empty(&test_head)){
+         list_for_each_safe(pos, q, &test_head){
 
-//             tmp= list_entry(pos, struct my_pid_data, list);
-//             // printk(KERN_INFO "Freeing List\n");
-//             list_del(pos);
-//             kfree(tmp);
-//          }
-//     }
-//    spin_unlock(&my_lock);
-//    printk(KERN_INFO "List Freed\n");
+            tmp= list_entry(pos, struct mp2_task_struct, list);
+            // printk(KERN_INFO "Freeing List\n");
+            list_del(pos);
+            kfree(tmp);
+         }
+    }
+   spin_unlock(&my_lock);
+   printk(KERN_ALERT "List Freed\n");
 
 //     // Removing the timer 
 //    printk(KERN_ALERT "removing timer\n");
