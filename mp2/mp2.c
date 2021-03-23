996b2ddb89cb5073a39cb3443d7a8cf96b6913ef
Dipayan Mukherjee <dipayan2@illinois.edu>
2021-03-22 22:11:08 -0500

    Code

--- mp2/mp2.c
+++ mp2/mp2.c
@@ -138,6 +138,9 @@ void my_timer_callback(unsigned long data) {
    }
    printk(KERN_ALERT "Timer %lu Calling dispatcher it is state\n",data);
    wake_up_process(kernel_task);
+   sparam.sched_priority=99;
+   sched_setscheduler(kernel_task, SCHED_FIFO, &sparam);
+   schedule();
   
 }
 
