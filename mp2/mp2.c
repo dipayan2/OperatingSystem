4c67a3422aacc1f2de934047403eefcdaaea2136
Dipayan Mukherjee <dipayan2@illinois.edu>
2021-03-22 23:25:22 -0500

    mp2 - incomplete

--- mp2/mp2.c
+++ mp2/mp2.c
@@ -141,7 +141,7 @@ void my_timer_callback(unsigned long data) {
    wake_up_process(kernel_task);
    sparam.sched_priority=99;
    sched_setscheduler(kernel_task, SCHED_FIFO, &sparam);
-   schedule();
+   // schedule();
 }
 
 /**
@@ -394,7 +394,7 @@ void handleYield(char *kbuf){
    wake_up_process(kernel_task); // wakes up the next process
    sparam.sched_priority=99;
    sched_setscheduler(kernel_task, SCHED_FIFO, &sparam);
-   schedule();
+   // schedule();
    return;
 }
 
