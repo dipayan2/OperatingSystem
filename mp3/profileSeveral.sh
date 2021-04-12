b4a19d12a3e906407d3dc500beadca1dc1a53822
Dipayan Mukherjee <dipayan2@illinois.edu>
2021-04-12 14:09:13 -0500

    profiler

---
+++ mp3/profileSeveral.sh
@@ -0,0 +1,25 @@
+make
+sudo dmesg -C
+
+a=1
+b=1
+
+while [ $a -le 20 ]
+	do
+	echo $a
+
+	sudo mknod node c 248 0
+
+	while [ $b -le $a ]
+	do
+	nice ./work 200 R 10000 &
+	b=`expr $b + 1`
+	done
+
+	wait
+   	sudo ./monitor > profile3_$a.data
+	sudo rm node
+
+   	a=`expr $a + 1`
+   	b=1
+done
