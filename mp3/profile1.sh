0e3eecd7db44a213da8386c04ae1d8118fd819e0
Dipayan Mukherjee <dipayan2@illinois.edu>
2021-04-12 14:36:11 -0500

    profiler

--- mp3/profile1.sh
+++ mp3/profile1.sh
@@ -1,5 +1,7 @@
+sudo mknod node c 248 0
 nice ./work 1024 R 50000 & nice ./work 1024 R 10000 &
 wait
 cat /proc/mp3/status
 pkill work
 sudo ./monitor > profile1.dat
+sudo rm node
--- mp3/profile2.sh
+++ mp3/profile2.sh
@@ -1,5 +1,7 @@
+sudo mknod node c 248 0
 nice ./work 1024 R 50000 & nice ./work 1024 L 10000 &
 wait
 cat /proc/mp3/status
 pkill work
 sudo ./monitor > profile2.dat
+sudo rm node
